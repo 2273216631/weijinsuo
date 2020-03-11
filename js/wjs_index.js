f486e203241f633d5567406a6e2f006387774087
2273216631 <2273216631@qq.com>
2020-03-11 15:31:58 +0800

    xinxiyvyuechanping

--- js/wjs_index.js
+++ js/wjs_index.js
@@ -2,6 +2,9 @@
  * Created by itcast.
  */
 $(function(){
+    /*初始化工具提示*/
+    $('[data-toggle="tooltip"]').tooltip();
+
     /*获取当前所有item*/
     var items=$(".carousel-inner .item");
     /*监听屏幕的大小改变*/
@@ -50,4 +53,25 @@ $(function(){
             carousel.carousel('next');
         }
     });
-}); 
+
+
+    /*计算产品块导航项的原始宽度*/
+    var ul=$(".wjs_product .nav-tabs");
+    var lis=ul.find("li");
+    var totalWidth=0;//总宽度
+    lis.each(function(index,value){
+        totalWidth=totalWidth+$(value).innerWidth();
+        console.log($(value).innerWidth());
+        /*获取宽度的方法的说明：
+        * width():它只能得到当前元素的内容的宽度
+        * innerWidth():它能获取当前元素的内容的宽度+padding
+        * outerWidth():获取当前元素的内容的宽度+padding+border
+        * outerWidth(true):获取元素的内容的宽度+padding+border+margin*/
+    });
+    ul.width(totalWidth);
+    /*使用插件实现导航条的滑动操作*/
+    var myScroll = new IScroll('.tabs_parent',{
+        /*设置水平滑动，不允许垂直滑动*/
+        scrollX: true, scrollY: false
+    });
+});
